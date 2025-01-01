a5d341af8f6e88ee774c97bbc68c9143045f9f1f
HardikPatel\hards <hardikpatel_7007@yahoo.co.in>
2025-01-01 22:38:34 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const PORT = 4000;
+
+
+app.get('/', (req, res) => {
+    res.status(200).json('Welcome, to Vercel Demo Test By - VDJHARDS');
+  });  
+
+app.get('/home', (req, res) => {
+  res.status(200).json('Welcome, your app is working well');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
+
+module.exports = app;
